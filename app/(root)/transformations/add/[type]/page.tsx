b9c2c444cd07d0b5494478a0f4046e94d558be9c
reeperx10 <acebany080@gmail.com>
2024-03-22 13:24:52 +0200

    initial commit

---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AddTransofrmationTypePage
+ = () => {
+  return (
+    <div>AddTransofrmationTypePage
+        
+    </div>
+  )
+}
+
+export default AddTransofrmationTypePage
